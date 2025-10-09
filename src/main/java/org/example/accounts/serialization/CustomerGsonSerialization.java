c7dec47938f2c4a5a9dd89cd57000df0629f0850
nesetrilovab <144663108+nesetrilovab@users.noreply.github.com>
2025-10-09 08:25:07 +0200

    xml serialization

--- src/main/java/org/example/accounts/serialization/CustomerGsonSerialization.java
+++ src/main/java/org/example/accounts/serialization/CustomerGsonSerialization.java
@@ -2,11 +2,12 @@ package org.example.accounts.serialization;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import org.example.people.customers.Customer;
 
 public class CustomerGsonSerialization implements Serialization {
     @Override
-    public String serialize(Object customer) throws JsonProcessingException {
+    public String serialize(Object customer) throws JsonSyntaxException {
         if (!(customer instanceof Customer)) {
             throw new RuntimeException("Customer object is not of type Customer");
         }
@@ -19,7 +20,7 @@ public class CustomerGsonSerialization implements Serialization {
     }
 
     @Override
-    public Customer deserialize(String serializedObject) throws JsonProcessingException {
+    public Customer deserialize(String serializedObject) throws JsonSyntaxException {
 
 
         GsonFactory gsonFactory = new GsonFactory();
